cdeac815d8e4aafd2f57c68dc844818afb221593
Utkir Khujaev <balkibumen@gmail.com>
2025-03-10 16:45:46 +0500

    update config example code

--- work/spark_utils.py
+++ work/spark_utils.py
@@ -4,8 +4,9 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext 
 
-sqlContext = SQLContext(spark)
-
 def get_spark():
     """Create and return a SparkSession."""
     return SparkSession.builder.appName("Pipezone").getOrCreate()
+
+spark = get_spark()
+# sqlContext = SQLContext(spark)
