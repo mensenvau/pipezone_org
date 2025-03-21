5bde8822a60aae88a939a2135d6e5f3a7cff96ca
Utkir Khujaev <balkibumen@gmail.com>
2025-03-21 21:48:51 +0500

    save v1

---
+++ workspace/.ipynb_checkpoints/spark_utils-checkpoint.py
@@ -0,0 +1,13 @@
+# type: ignore
+# workspace/spark_utils.py
+
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext 
+
+def get_spark():
+    """Create and return a SparkSession."""
+    return SparkSession.builder.appName("Pipezone").getOrCreate()
+
+spark = get_spark()
+sqlContext = SQLContext(spark)
+
